9f98d19aafa7fc369543549880c0bcf9b331a9de
muhfar <36448231+muhfar@users.noreply.github.com>
2019-02-13 22:16:13 +0700

    Initial commit

---
+++ application/views/Home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>	
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/mystyle.css');?>">
+
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"> -->
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
+
+  	<script type="text/javascript">
+  		$(window).on('scroll', function(){
+  			if ($(window).scrollTop()) {
+  				$('nav').addClass('black');
+  			}else{
+  				$('nav').removeClass('black');
+  			}
+  		})
+  	</script>
+
+<body>
+	<div class="wrapper">
+		<nav>
+			<div class="logo">LOGO</div>
+			<ul>
+				<li><a class="active" href="Beranda">Beranda</a></li>
+				<li><a href="Profil">Profil</a></li>
+				<li><a href="Struktur">Struktur</a></li>
+				<li><a href="Pencapaian">Pencapaian</a></li>
+			</ul>	
+		</nav>
+		
+	</div>
+
+	<section class="sec1"></section>
+	<section class="content">
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+	</section>
+
+	<!-- <div class="jumbotron text-center">
+		<img src="assets/Logo.png">
+		<h1>Selamat Datang di Sekolah Menengah Kejuruan X</h1>
+		<p>Lorem ipsum dolor sit amet..</p>
+		<p>Lorem ipsum dolor sit amet..</p>
+	</div> -->
+	
+
+
+
+
+
+
+</body> 
+</html> 
